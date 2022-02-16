085548068ea3441897c01a3889c4b6c362cc5a7e
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-16 15:36:06 -0500

    Add table and queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,33 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * 
+from animals
+WHERE name like '%mon' ;
+
+select name
+from animals
+where date_of_birth between 'jan-01-2016' and 'dec-31-2019';
+
+select name
+from animals
+where (neutered = true and escape_attemps < 3);
+
+select date_of_birth
+from animals
+where name in ('Agumon','Pikachu');
+
+select name, escape_attemps
+from animals
+where weight_kg > 10.5;
+
+select *
+from animals
+where neutered=true;
+
+select *
+from animals
+where name != 'Gabumon';
+
+select *
+from animals
+where (weight_kg between 10.4 and 17.3);
