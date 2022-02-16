f1bab8c8ed139ddc37f1dbfbcb0e2cfdec269236
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-16 14:14:31 -0500

    add queries

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, gender, dateborn, weight, neutered) VALUES ('Charmander', 'male','feb-08-2020', 11, false);
+INSERT INTO animals (name, gender, dateborn, weight, neutered, tries) VALUES ('Plantmon', 'female', 'nov-15-2022', 5.7, true, 2);
+INSERT INTO animals (name, gender, dateborn, weight, neutered, tries) VALUES ('Squirtle', 'male', 'apr-02-1993', 12.13, false, 3);
+INSERT INTO animals (name, gender, dateborn, weight, neutered, tries) VALUES ('Angemon', 'male', 'jun-12-2005', 45, true, 1);
+INSERT INTO animals (name, gender, dateborn, weight, neutered, tries) VALUES ('Boarmon', 'male', 'jun-07-2005', 20.4, true, 7);
+INSERT INTO animals (name, gender, dateborn, weight, neutered, tries) VALUES ('Blossom', 'female', 'oct-13-1998', 17, true, 3);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,90 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+begin;
+update animals set species = 'unspecified';
+table animals;
+rollback;
+table animals;
+
+begin;
+update animals
+set species = 'digimon'
+where name like '%mon';
+table animals;
+update animals
+set species = 'pokemon'
+where species is null;
+commit;
+table animals;
+
+begin;
+delete from animals;
+table animals;
+rollback;
+table animals;
+
+begin;
+delete from animals
+where dateborn > 'jan-01-2022';
+savepoint deleteold;
+update animals set weight = weight*(-1);
+table animals;
+rollback to deleteold;
+table animals;
+update animals set weight = weight*(-1)
+where weight < 0;
+table animals;
+commit;
+table animals;
+
+-- How many animals are there? = 5
+select count(*)
+from animals;
+
+-- How many animals have never tried to escape? = 1
+select count(*)
+from animals
+where tries = 0;
+
+-- What is the average weight of animals? = 21.106kg
+select avg(weight)
+from animals;
+
+-- Who escapes the most, neutered or not neutered animals? = neutered
+-- neutered animals escaped 3 times
+select count(*)
+from animals
+where (neutered = true and tries > 0);
+
+-- non neutered animals escaped 1 time
+select count(*)
+from animals
+where (neutered = false and tries > 0);
+
+-- What is the minimum and maximum weight of each type of animal?
+-- max weight pokemon = 17kg
+select max(weight)
+from animals
+where (species = 'pokemon');
+-- min weight pokemon = 11kg
+select min(weight)
+from animals
+where (species = 'pokemon');
+-- max weight digimon = 45kg
+select max(weight)
+from animals
+where (species = 'digimon');
+-- min weight digimon = 20.4
+select min(weight)
+from animals
+where (species = 'digimon');
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+-- pokemon's average attempts to escape = 3
+select avg(tries)
+from animals
+where (dateborn between 'jan-01-1990' and 'jan-01-2000' and species = 'pokemon');
+-- digimon's average attempts to escape = 0
+select avg(tries)
+from animals
+where (dateborn between 'jan-01-1990' and 'jan-01-2000' and species = 'digimon');
