76b8d27ee533763fbe33a2f5eb0b25ae0419eefa
Anjacodes <anja_schmidt7@posteo.de>
2022-02-21 14:56:10 -0600

    Query for Specific Vet ID

--- queries.sql
+++ queries.sql
@@ -1,5 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 -- Query one:
-CREATE INDEX animal_id_asc ON visits(animal_id ASC);
 explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
